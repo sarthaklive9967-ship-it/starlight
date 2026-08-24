658f72819ee9d2665c726a0a84e1098adc34d845
sarthaklive9967-ship-it <sarthaklive9967@gmail.com>
2026-08-24 20:58:23 +0530

    Add browser voice input and speech adapters

---
+++ src/lib/voice/index.ts
@@ -0,0 +1,4 @@
+export { getSpeechOutputCapabilities, getVoiceInputCapabilities } from './capabilities';
+export { SpeechOutputAdapter } from './speech-output-adapter';
+export { VoiceInputAdapter } from './voice-input-adapter';
+export type * from './types';
